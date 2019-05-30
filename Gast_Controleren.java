1976b896abc87e0906fc8b70c359bace81fe7b36
18064728 <47145360+18064728@users.noreply.github.com>
2019-05-30 16:01:06 +0200

    Update Gast_Controleren.java

--- Gast_Controleren.java
+++ Gast_Controleren.java
@@ -1,10 +1,13 @@
 package fullhouse;
 
 import javax.swing.*;
-import java.awt.*;
 import java.sql.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+/**
+ * Medewerker krijgt een overzicht van alle gasten en heeft de mogelijkheid om ze aan te passen of te verwijderen
+ */
 
 public class Gast_Controleren extends JDialog {
 
@@ -12,10 +15,6 @@ public class Gast_Controleren extends JDialog {
     private static final int width = 750;
     private static final int height = 500;
     private static final String title = "Gast Controleren";
-
-    //DATABASE
-    private Connection connection;
-    private PreparedStatement ps;
 
     //ATTRIBUTES
     private int ID;
@@ -28,13 +27,17 @@ public class Gast_Controleren extends JDialog {
     private String telefoonnummer;
     private String email;
 
+    private DefaultListModel<Gast> model;
+    private JScrollPane sp;
+    protected JList<Gast> gasten;
+    private Gast gast;
+
     //COMPONENTS
-    private JTextField textField;
-    private JList<String>gasten;
-    private JTable table;
+    private JButton bewerkBtn;
+    private JButton verwijderBtn;
+    private JButton terugBtn;
 
     public static void main(String[] args) {
-
         JDialog d = new Gast_Controleren();
         d.setSize(width, height);
         d.setTitle(title);
@@ -48,60 +51,158 @@ public class Gast_Controleren extends JDialog {
         panel.setLayout(null);
 
         try { //tries to make a connection with the database
-            connection = DriverManager.getConnection(
-                    "jdbc:mysql://meru.hhs.nl/18064728?useLegacyDatetimeCode=false&serverTimezone=Europe/Amsterdam",
-                    "leerlingnr", "password");
-
-            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * from gast");
+            ResultSet rs = ConnectionManager.getConnection().createStatement().executeQuery("SELECT * FROM gast");
             System.out.println("connected to database");
 
-            String[] columnNames = { "ID", "naam", "geslacht", "geboortedatum", "adres", "postcode", "woonplaats", "telefoonnummer", "email"};
-            String[][] data = {
-                    { "001", "Piet", "man", "2000-01-01", "straat", "1234ab", "den haag", "0612345678", "piet@email.com" },
-                    { "002", "Kees", "man", "1999-12-31", "straat2", "5678cd", "den haag", "0687654321", "kees@email.com" }
-            };
+            model = new DefaultListModel<>();
+            gasten = new JList<>(model);
+            panel.add(gasten);
+            sp = new JScrollPane(gasten);
+            sp.setBounds(125,25,500,400);
+            panel.add(sp);
 
-            table = new JTable(data, columnNames);
-            panel.add(table);
+            while (rs.next()) {
+                ID = rs.getInt("ID");
+                naam = rs.getString("naam");
+                geslacht = rs.getString("geslacht");
+                geboortedatum = rs.getDate("geboortedatum");
+                adres = rs.getString("adres");
+                postcode = rs.getString("postcode");
+                woonplaats = rs.getString("woonplaats");
+                telefoonnummer = rs.getString("telefoonnummer");
+                email = rs.getString("email");
 
-            JScrollPane j = new JScrollPane(table);
-            j.setBounds(0,50,750,500);
-            panel.add(j);
-
-            while (resultSet.next()) {
-                ID = resultSet.getInt("ID");
-                naam = resultSet.getString("naam");
-                geslacht = resultSet.getString("geslacht");
-                geboortedatum = resultSet.getDate("geboortedatum");
-                adres = resultSet.getString("adres");
-                postcode = resultSet.getString("postcode");
-                woonplaats = resultSet.getString("woonplaats");
-                telefoonnummer = resultSet.getString("telefoonnummer");
-                email = resultSet.getString("email");
-
-                System.out.println(resultSet.getInt("ID"));
-                System.out.println(resultSet.getString("naam"));
-                System.out.println(resultSet.getString("geslacht"));
-                System.out.println(resultSet.getString("geboortedatum"));
-                System.out.println(resultSet.getString("adres"));
-                System.out.println(resultSet.getString("postcode"));
-                System.out.println(resultSet.getString("woonplaats"));
-                System.out.println(resultSet.getString("telefoonnummer"));
-                System.out.println(resultSet.getString("email"));
-                System.out.println();
-
-                /*
-                System.out.format("%s, %s, %s, %s, %s, %s, %s, %s, %s\n", "ID", "Naam", "Geslacht", "geboortedatum", "adres", "postcode", "woonplaats", "telefoonnummer", "email");
-                System.out.println(resultSet.getString("naam"));
-                System.out.println(resultSet.getString("author"));
-                System.out.println(resultSet.getString("title"));
-                System.out.println();*/
-
+                gast = new Gast(ID, naam, geslacht, geboortedatum, adres, postcode, woonplaats, telefoonnummer, email);
+                model.addElement(gast);
             }
-
         } catch (SQLException e) { //error message if there's no connection with the database
             System.out.println("can't connect to the database, please contact tech-support");
+            e.printStackTrace();
         }
+
+        bewerkBtn = new JButton("bewerken");
+        terugBtn = new JButton("terug");
+        verwijderBtn = new JButton("verwijder");
+
+        bewerkBtn.setBounds(0,0,100,30);
+        terugBtn.setBounds(0,55,100,30);
+        verwijderBtn.setBounds(0,110,100,30);
+
+        panel.add(bewerkBtn);
+        panel.add(terugBtn);
+        panel.add(verwijderBtn);
+
+        ActionListener terug = new Terug();
+        ActionListener verwijder = new Verwijder(gasten);
+        ActionListener bewerk = new Bewerk(gasten);
+
+        bewerkBtn.addActionListener(bewerk);
+        terugBtn.addActionListener(terug);
+        verwijderBtn.addActionListener(verwijder);
+
         add(panel);
+    } 
+    
+    /**
+     * gaat terug naar class 'Gasten'
+     */
+    class Terug implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dispose();
+            new Gasten();
+        }
+    }
+
+    /**
+     * opent formulier met gegevens van gekozen gast zodat die bewerkt kan worden
+     */
+    class Bewerk implements ActionListener {
+        private JList<Gast> list;
+
+        public Bewerk(JList<Gast> list) {
+            this.list = list;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                if (list.isSelectionEmpty()) {
+                    JOptionPane.showMessageDialog(null, "kies een gast om te bewerken");
+                } else {
+                    ID = list.getSelectedValue().getID();
+                    naam = list.getSelectedValue().getNaam();
+                    geslacht = list.getSelectedValue().getGeslacht();
+                    geboortedatum = list.getSelectedValue().getGeboortedatum();
+                    adres = list.getSelectedValue().getAdres();
+                    postcode = list.getSelectedValue().getPostcode();
+                    woonplaats = list.getSelectedValue().getWoonplaats();
+                    telefoonnummer = list.getSelectedValue().getTelefoonnummer();
+                    email = list.getSelectedValue().getEmail();
+
+                    /**
+                     * new form with values from selected Gast
+                     */
+                    class Gast_Bewerken1 extends Gast_Toevoegen {
+
+                        public Gast_Bewerken1() {
+                            naamTxt.setText(list.getSelectedValue().getNaam());
+                            if (list.getSelectedValue().getGeslacht().equals("man")) {
+                                man.setSelected(true);
+                            } else if (list.getSelectedValue().getGeslacht().equals("vrouw")) {
+                                vrouw.setSelected(true);
+                            }
+                            adresTxt.setText(list.getSelectedValue().getAdres());
+                            //geboortedatum moet nog pre-selected worden
+                            postcodeTxt.setText(list.getSelectedValue().getPostcode());
+                            woonplaatsTxt.setText(list.getSelectedValue().getWoonplaats());
+                            telefoonnummerTxt.setText(list.getSelectedValue().getTelefoonnummer());
+                            emailTxt.setText(list.getSelectedValue().getEmail());
+
+                            ActionListener terug = new Terug();
+                            terugBtn.addActionListener(terug);
+                        }
+
+                        class Terug implements ActionListener { //listener werk niet goed omdat het class 'Gast_Toevoegen' extend
+                            public void actionPerformed(ActionEvent e) {
+                                dispose();
+                            }
+                        }
+                    }
+                    JDialog d = new Gast_Bewerken1();
+                    d.setSize(500, 500);
+                    d.setTitle("gast bewerken");
+                    d.setResizable(false);
+                    d.setVisible(true);
+                    d.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+                }
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * verwijderd gast van database
+     */
+    class Verwijder implements ActionListener {
+        private JList<Gast> list;
+        public Verwijder(JList<Gast> list) {
+            this.list = list;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                if (list.isSelectionEmpty()) {
+                    JOptionPane.showMessageDialog(null,"kies een gast om te bewerken");
+                } else {
+                    int ID = list.getSelectedValue().getID();
+                    ConnectionManager.getConnection().createStatement().execute("DELETE FROM gast WHERE id = " + ID);
+                    model.removeElement(list.getSelectedValue());
+                }
+            } catch (SQLException e1){
+                    e1.printStackTrace();
+            }
+        }
     }
 }
+
