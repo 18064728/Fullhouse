2d61f26756d02d555dcce1fab009bf58a02e9880
18064728 <47145360+18064728@users.noreply.github.com>
2019-05-30 17:09:22 +0200

    Add files via upload

---
+++ Masterclass.java
@@ -0,0 +1,89 @@
+package fullhouse;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * geeft de medewerker de mogelijkheid om een masterclass toe te voegen of een overzicht van te krijgen
+ * of om terug te gaan naar 'FullHouse'
+ */
+public class Masterclass extends JDialog {
+
+    private static final int width = 300;
+    private static final int height = 400;
+    private static final String title = "masterclass";
+
+    public static void main(String[] args) {
+        new Masterclass();
+    }
+
+    public Masterclass() {
+        addComponents();
+        setSize(width, height);
+        setTitle(title);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void addComponents() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+
+        JButton toevoegenBtn = new JButton("toevoegen");
+        JButton controlerenBtn = new JButton("controleren");
+        JButton terugBtn = new JButton("terug");
+
+        Dimension minSize = new Dimension(100, 50);
+        Dimension prefSize = new Dimension(100, 50);
+        Dimension maxSize = new Dimension(Short.MAX_VALUE, 50);
+
+        panel.add(new Box.Filler(minSize, prefSize, maxSize));
+        panel.add(toevoegenBtn);
+        panel.add(new Box.Filler(minSize, prefSize, maxSize));
+        panel.add(controlerenBtn);
+        panel.add(new Box.Filler(minSize, prefSize, maxSize));
+        panel.add(terugBtn);
+
+        /**
+         * gaat terug naar 'FullHouse'
+         */
+        class Terug implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                new FullHouse();
+                dispose();
+            }
+        }
+
+        /**
+         * opent nieuw JDialog met formulier om een masterclass toe te voegen
+         */
+        class Toevoegen implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                //todo add code
+                //todo nieuwe Masterclass_Toevoegen class implementeren
+            }
+        }
+
+        /**
+         * opent nieuw JDialog met een overzicht van alle masterclasses
+         */
+        class Controleren implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                //todo add code
+                //todo nieuwe Masterclass_Controleren class implementeren
+            }
+        }
+
+        ActionListener terug = new Terug();
+        ActionListener toevoegen = new Toevoegen();
+        ActionListener controleren = new Controleren();
+
+        terugBtn.addActionListener(terug);
+        toevoegenBtn.addActionListener(toevoegen);
+        controlerenBtn.addActionListener(controleren);
+
+        add(panel);
+    }
+}
