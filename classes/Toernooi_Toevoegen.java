4cb3478b0e1f3469875040ee67a0ce0070bbe365
18064728 <47145360+18064728@users.noreply.github.com>
2019-06-04 19:03:14 +0200

    Update Toernooi_Toevoegen.java

--- classes/Toernooi_Toevoegen.java
+++ classes/Toernooi_Toevoegen.java
@@ -250,9 +250,6 @@ public class Toernooi_Toevoegen extends JDialog {
         //gaat terug naar 'Toernooien'
         class Terug implements ActionListener {
             public void actionPerformed(ActionEvent e) {
-                JDialog d = new Toernooien();
-                d.setVisible(true);
-                d.setSize(300,400);
                 dispose();
             }
         }
