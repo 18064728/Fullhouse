b4b7ca42916b134ac78e15a62f75c38713bfea46
DanielZ94 <44162011+DanielZ94@users.noreply.github.com>
2019-06-07 21:11:39 +0200

    Add files via upload

---
+++ MasterclassInschrijvingControleren.java
@@ -0,0 +1,163 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MasterclassInschrijvingControleren extends JDialog {
+
+    //ATTRIBUTES
+    int inschrijvingnr;
+    int gastID;
+    int masterclassID;
+    String heeftBetaald;
+//    String heeftRating;
+
+    //COMPONENTS
+    private DefaultListModel<MasterclassInschrijving> model;
+    private JList<MasterclassInschrijving> inschrijvingen;
+
+    private JButton wijzigen;
+    private JButton verwijderen;
+    private JButton terug;
+//    private JButton welBetaald;
+//    private JButton nietBetaald;
+//    private JButton welRating;
+//    private JButton nietRating;
+
+    public MasterclassInschrijvingControleren() {
+
+        addComponents(); //zorgt ervoor dat alle components op het scherm komen
+        addList(); //zorgt ervoor dat de list gevuld word
+
+        class Wijzig implements ActionListener {
+            private JList<MasterclassInschrijving> list;
+
+            private Wijzig(JList<MasterclassInschrijving> list) {
+                this.list = list;
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (list.isSelectionEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Kies een gast om zijn betaling te wijzigen");
+                    } else {
+                        inschrijvingnr = list.getSelectedValue().inschrijvingnr;
+                        heeftBetaald = list.getSelectedValue().heeftBetaald;
+
+                        if (heeftBetaald.equals("nee")) { //wijzigt heeftBetaald naar ja
+                            PreparedStatement ps = ConnectionManager.getConnection().prepareStatement("UPDATE masterclass_inschrijving SET heeft_betaald = 'ja' where inschrijvingnr = ?;");
+                            ps.setInt(1, inschrijvingnr);
+                            ps.executeUpdate();
+                            model.removeAllElements();
+                            addList();
+                            JOptionPane.showMessageDialog(null,"betaling is gewijzigd");
+
+                        } else if (heeftBetaald.equals("ja")) { //wijzigt heeftBetaald naar nee
+                            PreparedStatement ps = ConnectionManager.getConnection().prepareStatement("UPDATE masterclass_inschrijving set heeft_betaald = 'nee' where inschrijvingnr = ?;");
+                            ps.setInt(1,inschrijvingnr);
+                            ps.executeUpdate();
+                            model.removeAllElements();
+                            addList();
+                            JOptionPane.showMessageDialog(null,"betaling is gewijzigd");
+                        }
+                    }
+                } catch (Exception ex) {
+                    System.out.println("er ging iets mis");
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+
+        class Verwijder implements ActionListener {
+            private JList<MasterclassInschrijving> list;
+
+            private Verwijder(JList<MasterclassInschrijving> list) {
+                this.list = list;
+            }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (list.isSelectionEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Kies een gast om te verwijderen");
+                    } else {
+                        inschrijvingnr = list.getSelectedValue().getInschrijvingnr();
+                        PreparedStatement ps = ConnectionManager.getConnection().prepareStatement("DELETE FROM masterclass_inschrijving WHERE inschrijvingnr = ?");
+                        ps.setInt(1, inschrijvingnr);
+                        ps.executeUpdate();
+                        model.removeAllElements();
+                        addList();
+                        JOptionPane.showMessageDialog(null,"inschrijving is verwijderd");
+                    }
+                } catch (Exception ex) {
+                    System.out.println("er ging iets mis");
+                    ex.printStackTrace();
+                }
+            }
+
+        }
+
+
+        ActionListener wijzig = new Wijzig(inschrijvingen);
+        ActionListener verwijder = new Verwijder(inschrijvingen);
+        wijzigen.addActionListener(wijzig);
+        verwijderen.addActionListener(verwijder);
+    }
+
+    public void addComponents() {
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        try {
+            model = new DefaultListModel<>();
+            inschrijvingen = new JList<>(model);
+            panel.add(inschrijvingen);
+            JScrollPane sp = new JScrollPane(inschrijvingen);
+            sp.setBounds(105, 25, 500, 400);
+            panel.add(sp);
+
+            wijzigen = new JButton("<html>betaling wijzigen</html>");
+            wijzigen.setToolTipText("klik op een gast en deze knop om zijn betaling te wijzigen");
+            wijzigen.setBounds(0, 5, 105, 50);
+            panel.add(wijzigen);
+
+            verwijderen = new JButton("verwijderen");
+            verwijderen.setBounds(0, 80, 105, 30);
+            panel.add(verwijderen);
+
+            terug = new JButton(new AbstractAction("terug") {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                }
+            });
+            terug.setBounds(0, 130, 105, 30);
+            panel.add(terug);
+
+        } catch (Exception e) {
+            System.out.println("er ging iets mis");
+            e.printStackTrace();
+        }
+        add(panel);
+    }
+
+    public void addList() {
+        try {
+            ResultSet rs = ConnectionManager.getConnection().createStatement().executeQuery("SELECT * FROM masterclass_inschrijving");
+            while (rs.next()) {
+                inschrijvingnr = rs.getInt(1);
+                gastID = rs.getInt(2);
+                masterclassID = rs.getInt(3);
+                heeftBetaald = rs.getString(4);
+
+                MasterclassInschrijving ti = new MasterclassInschrijving(inschrijvingnr, gastID, masterclassID, heeftBetaald);
+                model.addElement(ti);
+            }
+        } catch (Exception e) {
+            System.out.println("er ging iets mis");
+            e.printStackTrace();
+        }
+    }
+}
